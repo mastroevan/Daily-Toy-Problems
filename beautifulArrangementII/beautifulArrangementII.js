c19b01c70b2346c462ae03029649296591ebcfcf
mastroevan <mastroevan@gmail.com>
2021-12-27 11:38:41 -0700

    beautiful arrangement II

---
+++ beautifulArrangementII/beautifulArrangementII.js
@@ -0,0 +1,32 @@
+/*Given two integers n and k, construct a list answer that contains n different positive integers ranging from 1 to n and obeys the following requirement:
+
+Suppose this list is answer = [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
+Return the list answer. If there multiple valid answers, return any of them.
+
+
+
+Example 1:
+
+Input: n = 3, k = 1
+Output: [1,2,3]
+Explanation: The [1,2,3] has three different positive integers ranging from 1 to 3, and the [1,1] has exactly 1 distinct integer: 1
+Example 2:
+
+Input: n = 3, k = 2
+Output: [1,3,2]
+Explanation: The [1,3,2] has three different positive integers ranging from 1 to 3, and the [2,1] has exactly 2 distinct integers: 1 and 2.
+*/
+
+
+
+var constructArray = function(n, k) {
+  const ans = Array(n);
+  let c = 0;
+  for (let v = 1; v < n - k; v++) {
+    ans[c++] = v;
+  }
+  for (let i = 0; i <= k; i++) {
+    ans[c++] = i % 2 === 0 ? n - k + Math.floor(i / 2) : n - Math.floor(i / 2);
+  }
+  return ans;
+}
