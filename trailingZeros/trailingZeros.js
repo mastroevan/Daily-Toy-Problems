367f101ca461767a1b9428a33d4d3ea0798a8a69
mastroevan <mastroevan@gmail.com>
2022-01-10 10:23:07 -0700

    inital idea

---
+++ trailingZeros/trailingZeros.js
@@ -0,0 +1,43 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+Example 1:
+  Input: n = 3
+  Output: 0
+  Explanation: 3! = 6, no trailing zero.
+
+Example 2:
+  Input: n = 5
+  Output: 1
+  Explanation: 5! = 120, one trailing zero.
+
+Example 3:
+  Input: n = 0
+  Output: 0
+
+Constraints:
+  0 <= n <= 104
+  */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  let count = 0;
+  let nStr = factorial(n).toString();
+  for (i = nStr.length - 1; i >= 0; i--) {
+    if (nStr[i] === '0') {
+      count++;
+    } else {
+      break;
+    }
+  }
+  return count;
+};
+
+const factorial = n => {
+  return n ? n * factorial(n - 1) : 1;
+}
+
