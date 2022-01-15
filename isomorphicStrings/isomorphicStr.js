ad141d63705fb9ae5a89406c531eb945a5e4277f
mastroevan <mastroevan@gmail.com>
2022-01-15 10:43:09 -0700

    refactored

--- isomorphicStrings/isomorphicStr.js
+++ isomorphicStrings/isomorphicStr.js
@@ -27,7 +27,8 @@ Constraints:
  * @param {string} t
  * @return {boolean}
  */
- var isIsomorphic = function(s, t) {
+
+var isIsomorphic = function(s, t) {
   let obj = {};
   for (let i = 0; i < s.length; i++) {
     if (!obj['s' + s[i]]) obj['s' + s[i]] = t[i];
@@ -35,4 +36,23 @@ Constraints:
     if (t[i] !== obj['s' + s[i]] || s[i] !== obj['t' + t[i]]) return false;
   }
   return true;
+};
+
+//refactored solution:
+var isIsomorphic = function(s, t) {
+  if (s.length != t.length)
+      return false
+  let m = new Map()
+  for (let i = 0; i < s.length; i++) {
+      if (!m.has(s[i]))
+          m.set(s[i], t[i])
+      else {
+
+          if (m.get(s[i]) != t[i]) {
+
+              return false
+          }
+      }
+  }
+  return new Set([...m.values()]).size == m.size
 };
