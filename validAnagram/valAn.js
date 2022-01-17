2e764df75bd11d2c647850132724d5f426e963b6
mastroevan <mastroevan@gmail.com>
2022-01-17 11:08:06 -0700

    inital solution

---
+++ validAnagram/valAn.js
@@ -0,0 +1,36 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+  Input: s = "anagram", t = "nagaram"
+  Output: true
+
+Example 2:
+  Input: s = "rat", t = "car"
+  Output: false
+
+Constraints:
+  1 <= s.length, t.length <= 5 * 104
+  s and t consist of lowercase English letters.
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ var isAnagram = function(s, t) {
+  let sLen = s.length;
+  let tLen = t.length;
+  if (sLen === 0 || tLen === 0) {
+    return false;
+  }
+  if (sLen !== tLen) {
+    return false;
+  }
+  let sCheck = s.split('').sort().join('')
+  let tCheck = t.split('').sort().join('');
+  if (sCheck === tCheck) return true;
+  return false;
+};
+
