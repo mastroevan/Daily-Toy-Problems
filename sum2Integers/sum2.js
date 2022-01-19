952b1edde4023c6225e0d18c6caf2d3b12558e50
mastroevan <mastroevan@gmail.com>
2022-01-19 10:16:23 -0700

    first attempt solution

---
+++ sum2Integers/sum2.js
@@ -0,0 +1,28 @@
+/*
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+Example 1:
+  Input: a = 1, b = 2
+  Output: 3
+Example 2:
+  Input: a = 2, b = 3
+  Output: 5
+
+Constraints:
+  -1000 <= a, b <= 1000
+*/
+
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+
+//Recursive Method:
+ var getSum = function(a, b) {
+  if (b === 0) {
+    return a;
+  }
+  return getSum((a ^ b), (a & b) << 1);
+};
