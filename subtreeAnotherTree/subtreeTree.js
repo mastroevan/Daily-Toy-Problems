057989c09ff9ad926ee68e54874730fe2addcdec
mastroevan <mastroevan@gmail.com>
2022-01-21 10:02:35 -0700

    hybrid version

--- subtreeAnotherTree/subtreeTree.js
+++ subtreeAnotherTree/subtreeTree.js
@@ -74,3 +74,20 @@ const isSubtree = (root, subRoot) => {
   }
   return false;
 };
+
+//A hybrid version of the two:
+const isSubtree = (root, subRoot) => {
+  const areEqual = (node1, node2) => {
+    if (!node1 || !node2) return !node1 && !node2;
+    if (node1.val !== node2.val) return false;
+    return areEqual(node1.left, node2.left) && areEqual(node1.right, node2.right);
+  }
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.pop();
+    if (!node) continue;
+    if (areEqual(node, subRoot)) return true;
+    queue.push(node.right, node.left);
+  }
+  return false;
+};
