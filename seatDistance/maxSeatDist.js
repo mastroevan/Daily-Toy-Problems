2c7236f21211bc0a80caf2bba142ed0f6bb7f201
mastroevan <mastroevan@gmail.com>
2022-01-28 09:49:33 -0700

    refactored solution, faster than 99.07 percent of JS submissions on leetcode

--- seatDistance/maxSeatDist.js
+++ seatDistance/maxSeatDist.js
@@ -32,6 +32,7 @@ Constraints:
  * @param {number[]} seats
  * @return {number}
  */
+/*
  var maxDistToClosest = function(seats) {
   let max = 0, count = 0, seated = false;
   for (let i = 0; i < seats.length; i++) {
@@ -45,3 +46,17 @@ Constraints:
   }
   return Math.max(count, max);
 };
+*/
+
+//Refactored Solution:
+function maxDistToClosest(seats) {
+  let left = seats.indexOf(1);
+  let max = left;
+  for (let right = left + 1; right < seats.length; right++) {
+    if (seats[right] === 0) {
+      continue;
+    }
+    [max, left] = [Math.max(max, ((right - left) / 2) | 0), right];
+  }
+  return Math.max(max, seats.length - left - 1);
+}
