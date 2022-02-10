e46965e1cea9471fde0129d82d7c97c03202401c
mastroevan <mastroevan@gmail.com>
2022-02-10 09:53:44 -0700

    iinital solution need to refactor

---
+++ rotateImage/rotateImg.js
@@ -0,0 +1,47 @@
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const swap = function (or, oc, tr, tc) {
+    [this[or][oc], this[tr][tc]] = [this[tr][tc], this[or][oc]];
+  };
+  let totalLevel = Math.floor(matrix.length / 2);
+  let last = matrix.length - 1;
+  let level = 0;
+  while (level < totalLevel) {
+    for (let i = level; i <= last - 1; i++) {
+      swap.apply(matrix, [level, i, i, last]); // swap from left top to right top
+      swap.apply(matrix, [level, i, last, last - i + level]);
+      swap.apply(matrix, [level, i, last - i + level, level]);
+    }
+    ++level;
+    --last;
+  }
+};
