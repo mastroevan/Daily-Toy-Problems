1cae7e5959b46058e1cf226ee986d6ecfaa137ad
mastroevan <mastroevan@gmail.com>
2022-03-09 08:24:37 -0700

    still need to refactor

---
+++ newYearsChaos/nYChaos.js
@@ -0,0 +1,99 @@
+/*
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+Function Description
+
+Complete the function minimumBribes in the editor below.
+
+minimumBribes has the following parameter(s):
+
+int q[n]: the positions of the people after all bribes
+Returns
+
+No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than 2 people.
+Input Format
+
+The first line contains an integer t, the number of test cases.
+
+Each of the next t pairs of lines are as follows:
+- The first line contains an integer t, the number of people in the queue
+- The second line has n space-separated integers describing the final state of the queue.
+
+
+Sample Input
+
+STDIN       Function
+-----       --------
+2           t = 2
+5           n = 5
+2 1 5 3 4   q = [2, 1, 5, 3, 4]
+5           n = 5
+2 5 1 3 4   q = [2, 5, 1, 3, 4]
+Sample Output
+
+3
+Too chaotic
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+function minimumBribes(queue) {
+    // Write your code here
+  let chaotic = false;
+  let bribes = 0;
+  for (let i = 0; i < queue.length; i++) {
+    if (queue[i] - (i+1) > 2) {
+      chaotic = true;
+    }
+    for (let j = queue[i] - 2; j < i; j++) {
+      if (queue[j] > queue[i]) {
+        bribes++;
+      }
+    }
+  }
+  if (chaotic === true) {
+    console.log('Too chaotic');
+  } else {
+    console.log(bribes);
+  }
+}
+
+function main() {
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const q = readLine().replace(/\s+$/g, '').split(' ').map(qTemp => parseInt(qTemp, 10));
+
+        minimumBribes(q);
+    }
+}
