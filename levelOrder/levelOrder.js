553dd8deb21de14c2e0d9fa0f30b15136151191e
mastroevan <mastroevan@gmail.com>
2022-03-14 13:19:52 -0600

    working solution but need to refactor / optimize

---
+++ levelOrder/levelOrder.js
@@ -0,0 +1,53 @@
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:
+  Input: root = [3,9,20,null,null,15,7]
+  Output: [[3],[9,20],[15,7]]
+Example 2:
+  Input: root = [1]
+  Output: [[1]]
+Example 3:
+  Input: root = []
+  Output: []
+
+Constraints:
+  The number of nodes in the tree is in the range [0, 2000].
+  -1000 <= Node.val <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  let result = [];
+  let currentLevelNodes = [];
+  if (root) {
+      currentLevelNodes.push(root);
+    }
+  while (currentLevelNodes.length > 0) {
+    let current = [];
+    let len = currentLevelNodes.length;
+    for (let i = 0; i < len; i++) {
+      let node = currentLevelNodes.shift();
+       current.push(node.val);
+       if (node.left) {
+        currentLevelNodes.push(node.left);
+       }
+       if (node.right) {
+         currentLevelNodes.push(node.right);
+       }
+    }
+    result.push(current);
+  }
+  return result;
+};
