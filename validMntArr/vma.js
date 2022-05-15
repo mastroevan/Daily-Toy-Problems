1167e3c67616899a62e116148bad9d5b41b0fec1
mastroevan <mastroevan@gmail.com>
2022-05-15 16:50:42 -0600

    optimized solution

--- validMntArr/vma.js
+++ validMntArr/vma.js
@@ -34,30 +34,50 @@ Constraints:
  * @param {number[]} arr
  * @return {boolean}
  */
- var validMountainArray = function(A) {
-  if(A.length < 3) {
-      return false;
-  }
-  let max = [0,0];
-  for(let i = 0; i < A.length; i++) {
-      if(A[i] > max[1]) {
-          max[0] = i;
-          max[1] = A[i];
-      }
-  }
-  if(max[0] == 0 || max[0] == A.length - 1){
-      return false;
-  }
-  for(let i = 0; i < max[0]; i++) {
-      if(A[i] >= A[i + 1]) {
-          return false;
-      }
-  }
+//  var validMountainArray = function(A) {
+//   if(A.length < 3) {
+//       return false;
+//   }
+//   let max = [0,0];
+//   for(let i = 0; i < A.length; i++) {
+//       if(A[i] > max[1]) {
+//           max[0] = i;
+//           max[1] = A[i];
+//       }
+//   }
+//   if(max[0] == 0 || max[0] == A.length - 1){
+//       return false;
+//   }
+//   for(let i = 0; i < max[0]; i++) {
+//       if(A[i] >= A[i + 1]) {
+//           return false;
+//       }
+//   }
 
-  for(let i = max[0] + 1; i < A.length; i++) {
-      if(A[i - 1] <= A[i]) {
-          return false;
-      }
+//   for(let i = max[0] + 1; i < A.length; i++) {
+//       if(A[i - 1] <= A[i]) {
+//           return false;
+//       }
+//   }
+//   return true;
+// };
+
+
+//one pass solution
+var validMountainArray = function(A) {
+  const n = A.length;
+  if (n < 3) return false;
+  let i = 1;
+  let rise = false;
+  let drop = false;
+  while (A[i - 1] < A[i]) {
+    i++;
+    rise = true;
   }
-  return true;
+  if (A[i - 1] === A[i]) return false;
+  while (A[i - 1] > A[i]) {
+    i++;
+    drop = true;
+  }
+  return i === n && rise && drop;
 };
