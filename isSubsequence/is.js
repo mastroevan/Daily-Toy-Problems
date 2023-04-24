b1ceeffe3cc595bc610890bbdc9c9a2c67ad3d0e
Evan Mastro <evan.mastro@ansolinc.com>
2023-04-23 21:28:33 -0700

    runtime 47ms, memory 41.7MB

---
+++ isSubsequence/is.js
@@ -0,0 +1,44 @@
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+
+*/
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if (s.length > t.length) {
+        return false;
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (!t.includes(s[i])) {
+            return false;
+        } else{
+            t = t.slice(t.indexOf(s[i]) +1,)
+        }
+    }
+    return true;
+};
